5bb3b1a4073675065559c38f75a25f16bf3aa2df
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 09:37:54 -0600

    Add ability for admin users to create new genres from the genre index page.

--- spec/features/user_sees_all_genres_spec.rb
+++ spec/features/user_sees_all_genres_spec.rb
@@ -16,10 +16,11 @@ describe "A user sees visits '/genres'" do
     end
   end
   describe "As an admin user" do
-    it 'they see the same information visitors see and a form to create a new genre' do
+    before :each do
       user = User.create(username: 'admin', password: 'password', role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+    end
+    it 'they see the same information visitors see and a form to create a new genre' do
       genre_1 = Genre.create(name: 'Rock')
       genre_2 = Genre.create(name: 'Funk')
       genre_3 = Genre.create(name: 'Disco')
@@ -36,6 +37,22 @@ describe "A user sees visits '/genres'" do
       end
     end
     it 'they can create a new genre by filling out the form' do
+      genre_1 = Genre.create(name: 'Rock')
+      genre_2 = Genre.create(name: 'Funk')
+      genre_3 = Genre.create(name: 'Disco')
+
+      visit genres_path
+
+      new_genre = 'Techno'
+
+      fill_in :genre_name, with: new_genre
+      click_on 'Create Genre'
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to have_content(new_genre)
+      expect(page).to have_content(genre_1.name)
+      expect(page).to have_content(genre_2.name)
+      expect(page).to have_content(genre_3.name)
     end
   end
 end
