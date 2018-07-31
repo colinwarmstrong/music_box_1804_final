ebdbd08494b7613ddfc5715a7135e80654bbf38b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 10:12:10 -0600

    Add ability for users to see all genres associated with a song on a song show page.

--- spec/features/user_can_see_song_show_spec.rb
+++ spec/features/user_can_see_song_show_spec.rb
@@ -24,5 +24,20 @@ describe 'user sees one song' do
 
     expect(page).to have_content("Rating: #{song_1.rating}")
   end
-  it 'they see all genres associ'
+  it 'they see all genres associated with a song' do
+    artist = Artist.create(name: 'Journey')
+    song = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808)
+    genre_1 = song.genres.create(name: 'Rock')
+    genre_2 = song.genres.create(name: 'Funk')
+    genre_3 = Genre.create(name: 'Punk')
+
+    visit song_path(song)
+
+    within('.song-genres') do
+      expect(page).to have_content('Genres for this Song:')
+      expect(page).to have_content(genre_1.name)
+      expect(page).to have_content(genre_2.name)
+      expect(page).to_not have_content(genre_3.name)
+    end
+  end
 end
