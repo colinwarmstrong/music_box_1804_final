34e59ab518cbe13ee184716215710ea76c89f478
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 10:32:29 -0600

    Refactor some of the model tests to be more clean and readable.

--- spec/models/song_genre_spec.rb
+++ spec/models/song_genre_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe SongGenre, type: :model do
-  describe 'validations' do
+  describe 'relationships' do
     it {should belong_to :song}
-    it {should belong_to :genre}    
+    it {should belong_to :genre}
   end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -5,11 +5,14 @@ describe Song, type: :model do
     it{should validate_presence_of(:title)}
     it{should validate_presence_of(:length)}
     it{should validate_presence_of(:play_count)}
-    it{should have_many :song_genres}
-    it{should have_many :genres}  
+
   end
 
   describe 'relationships' do
     it{should belong_to(:artist)}
+    it{should have_many :song_genres}
+    it{should have_many :genres}
   end
+
+  describe 'ist'
 end
