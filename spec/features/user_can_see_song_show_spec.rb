0da424f2f50026c9c9f8c40287d6ddbf211550f5
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 09:13:05 -0600

    User can see a song rating on a song show page.

--- spec/features/user_can_see_song_show_spec.rb
+++ spec/features/user_can_see_song_show_spec.rb
@@ -15,4 +15,13 @@ describe 'user sees one song' do
     expect(page).to_not have_content(song_2.title)
     expect(page).to_not have_content(song_2.length)
   end
+  it 'with numeric rating for this song' do
+    artist = Artist.create(name: 'Journey')
+    song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 1)
+    song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908, rating: 5)
+
+    visit song_path(song_1)
+
+    expect(page).to have_content("Rating: #{song_1.rating}")
+  end
 end
