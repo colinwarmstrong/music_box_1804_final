674f0f0f7abf6637f8d0f782b083c91de926b5b6
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 10:45:07 -0600

    Add ability for users to see up to three other songs with the same rating on the song show page.

--- spec/features/user_can_see_song_show_spec.rb
+++ spec/features/user_can_see_song_show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'user sees one song' do
   it 'with title and length' do
     artist = Artist.create(name: 'Journey')
-    song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808)
-    song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908)
+    song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 3)
+    song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908, rating: 4)
 
     visit song_path(song_1)
 
@@ -38,6 +38,27 @@ describe 'user sees one song' do
       expect(page).to have_content(genre_1.name)
       expect(page).to have_content(genre_2.name)
       expect(page).to_not have_content(genre_3.name)
+    end
+  end
+  it 'they see up to three other songs that have the same rating' do
+    artist = Artist.create(name:'Adele')
+    song_1 = artist.songs.create(title: "Don't Stop Believin", length: 303, play_count: 123456, rating: 4)
+    song_2 = artist.songs.create(title: "Never Gonna Give You Up", length: 253, play_count: 98762452, rating: 4)
+    song_3 = artist.songs.create(title: "Everlong", length: 333, play_count: 98762452, rating: 4)
+    song_4 = artist.songs.create(title: "Pretenders", length: 283, play_count: 98762452, rating: 4)
+    song_5 = artist.songs.create(title: "Hero", length: 423, play_count: 98762452, rating: 4)
+    song_6 = artist.songs.create(title: "Monkey Wrench", length: 323, play_count: 98762452, rating: 3)
+
+    visit song_path(song_1)
+
+    within '.songs-with-same-rating' do
+      expect(page).to have_content('Songs With Same Rating:')
+      expect(page).to have_link(song_2.title)
+      expect(page).to have_link(song_3.title)
+      expect(page).to have_link(song_4.title)
+
+      expect(page).to_not have_content(song_5.title)
+      expect(page).to_not have_content(song_6.title)
     end
   end
 end
