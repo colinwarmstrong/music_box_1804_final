5d6945f624ae662e20cdba8f1ad27d8e315d44d1
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 09:29:32 -0600

    Add form to genre index that can be accessed by admin users.

--- spec/features/user_sees_all_genres_spec.rb
+++ spec/features/user_sees_all_genres_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
-describe "A visitor visits '/genres'" do
-  it 'they see all genres in the database' do
-    genre_1 = Genre.create(name: 'Rock')
-    genre_2 = Genre.create(name: 'Funk')
-    genre_3 = Genre.create(name: 'Disco')
+describe "A user sees visits '/genres'" do
+  describe "As a visitor" do
+    it 'they see all genres in the database' do
+      genre_1 = Genre.create(name: 'Rock')
+      genre_2 = Genre.create(name: 'Funk')
+      genre_3 = Genre.create(name: 'Disco')
 
-    visit genres_path
+      visit genres_path
 
-    expect(page).to have_content(genre_1.name)
-    expect(page).to have_content(genre_2.name)
-    expect(page).to have_content(genre_3.name)
+      expect(page).to have_content(genre_1.name)
+      expect(page).to have_content(genre_2.name)
+      expect(page).to have_content(genre_3.name)
+      expect(page).to_not have_content('Create New Genre')
+    end
+  end
+  describe "As an admin user" do
+    it 'they see the same information visitors see and a form to create a new genre' do
+      user = User.create(username: 'admin', password: 'password', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      genre_1 = Genre.create(name: 'Rock')
+      genre_2 = Genre.create(name: 'Funk')
+      genre_3 = Genre.create(name: 'Disco')
+
+      visit genres_path
+
+      expect(page).to have_content(genre_1.name)
+      expect(page).to have_content(genre_2.name)
+      expect(page).to have_content(genre_3.name)
+      expect(page).to have_content('Create New Genre')
+    end
+    it 'they can create a new genre by filling out the form' do
+    end
   end
 end
